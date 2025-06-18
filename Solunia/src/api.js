6736822f53f9b64dabbd92aaeca3456b19c45d95
GonzaloG28 <gonzalogimenez280@gmail.com>
2025-06-17 20:49:51 -0400

    error

--- Solunia/src/api.js
+++ Solunia/src/api.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000";
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export async function obtenerRespuestaIA({ message, style }) {
   const res = await fetch(`${BASE_URL}/api/openai/respuesta`, {
